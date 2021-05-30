22c01f0cf8c24c3ccb9ff336e447d311faebf203
Hanam Do <hanam.do@gmail.com>
2021-05-30 15:25:37 -0700

    Removed starter code to start with a blank slate

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const text = screen.getByText(/Hello, Interviewers!/i);
+  expect(text).toBeInTheDocument();
 });
